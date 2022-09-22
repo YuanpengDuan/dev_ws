f25930492e757bfe8604e49594d9fd9617276066
YuanpengDuan <97944346+YuanpengDuan@users.noreply.github.com>
2022-09-22 11:10:29 +0800

    V1.1

---
+++ dev_ws/src/planning/include/planning/ntzx_qianxun_inv.hpp
@@ -0,0 +1,74 @@
+/********************************************************************
+    * Copyright (C), 2021, 南通智行未来车联网创新中心有限公司
+    * 文件名：ntzx_qianxun_inv.h
+    * 作者：任家豪
+    * 日期：2021年2月22日
+    * 描述：千寻惯导的初始化，以及一些对外封装
+ * ******************************************************************/
+
+#ifndef _NTZX_QIANXUN_INV_H_
+#define _NTZX_QIANXUN_INV_H_
+
+#define NTZX_QIANXUN_SUCCESS        0
+#define NTZX_QIANXUN_PARA_ERR       (-61)
+#define NTZX_QIANXUN_CONF_ERR       (-62)
+#define NTZX_QIANXUN_DELAY_SYNC     (-63)
+#define NTZX_QIANXUN_ANALYSIS_ERR   (-64)
+#define NTZX_QIANXUN_STATE_ERR      (-65)
+#define NTZX_QIANXUN_RECV_ERR       (-66)
+#define NTZX_QIANXUN_DATA_INVALID   (-66)
+#define NTZX_QIANXUN_DATA_ERR       (-67)
+
+#define NTZX_QIANXUN_QUAL_NONE      0
+#define NTZX_QIANXUN_QUAL_SINGLE    1
+#define NTZX_QIANXUN_QUAL_SBAS      2
+#define NTZX_QIANXUN_QUAL_RTKFIX    4
+#define NTZX_QIANXUN_QUAL_RTKFLOAT  5
+#define NTZX_QIANXUN_QUAL_INS       6
+#define NTZX_QIANXUN_QUAL_USERS     7
+
+#pragma pack(push, 1)
+typedef struct gps_time {
+    int hour;  // 当条gps数据的小时
+    int min;   // 当条gps数据的分钟
+    int s;     // 当条gps数据的秒数
+    int ms;    // 当条gps数据的毫秒
+} str_gps_time;
+
+typedef struct gps_data {
+    int frame;
+    str_gps_time time;      // 当条数据的北京时间
+    double lat;             // 纬度
+    unsigned char lat_dir;   // 北纬，还是南纬
+    double lon;              // 经度
+    unsigned char lon_dir;   // 西经，东经
+    unsigned char qual;      // GPS质量指标
+    double courseAngle;      // 航向角度值
+     double Picth;            //俯仰角
+    unsigned char u_unit;    // 地理高度单位m
+} str_gps_data;
+
+typedef struct vel_date_data{
+    float Vel_N;
+    float Vel_E;
+    float Vel_D;
+    float Vel_G;
+
+}str_vel_data;
+
+
+typedef struct inv_to_pl {
+    int frame;
+    double lat;             // 纬度
+    double lon;             // 经度
+    double courseAngle; // 航向角度值
+    double speedmm;//后加的
+} str_inv_to_pl;
+#pragma pack(pop)
+
+void *ntzx_qianxun_inv_main(void *arg); // 线程主函数
+int ntzx_get_inv_to_pl(str_inv_to_pl *nav_info); // 提供给PL的导航信息
+int ntzx_qianxun_init(void);
+void qianxun_flush(void);   //刷新千寻接口缓冲
+#endif
+
---
+++ dev_ws/src/planning/src/ntzx_fuse_data.cpp
@@ -0,0 +1,102 @@
+/********************************************************************
+ * Copyright (C), 2021, 南通智行未来车联网创新中心有限公司
+ * 文件名：ntzx_qianxun_inv.h
+ * 作者：任家豪
+ * 日期：2021年2月26日
+ * 描述：数据融合功能函数
+ * ******************************************************************/
+#include "planning/ntzx_fuse_data.hpp"
+#include "planning/ntzx_log_app.hpp"
+#include "planning/ntzx_lidar_leishen.hpp"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+static str_lidar_info_to_fuse g_fuse_lidar = {0};
+static str_grid_fuse_to_pl g_grid_fuse_to_pl = {0};
+static int g_proc_flag = 0;
+static pthread_mutex_t g_mutex_to_pl = PTHREAD_MUTEX_INITIALIZER;//用于控制收发UDP包的线程
+
+static void *ntzx_fuse_data_recv(void *arg);
+static void *ntzx_fuse_data_proc(void *arg);
+
+void *ntzx_fuse_data_main(void *arg)
+{
+    int rt;
+    /* 创建两个线程，一个接收线程，一个处理线程 */
+    pthread_t fuse_data_recv;
+    pthread_t fuse_data_pro;
+    rt = pthread_create(&fuse_data_recv, NULL, ntzx_fuse_data_recv, NULL);
+    if (rt < 0) {
+        ntzx_stereotypes_log_write_buf("fuse creat recv thread err!!!");
+    }
+    rt = pthread_create(&fuse_data_pro, NULL, ntzx_fuse_data_proc, NULL);
+    if (rt < 0) {
+        ntzx_stereotypes_log_write_buf("fuse creat recv thread err!!!");
+    }
+    pthread_join(fuse_data_recv, NULL);
+    pthread_join(fuse_data_pro, NULL);
+}
+
+static void *ntzx_fuse_data_recv(void *arg)
+{
+    int rt = 0;
+    str_lidar_info_to_fuse fuse_lidar = {0};
+    while (1) {
+        /* 获取激光雷达数据 */
+        rt = ntzx_lidar_leishen_get_data(&fuse_lidar);
+        if (rt < 0) {
+            usleep(1000);
+            continue;
+        }
+        while (g_proc_flag != 0) {
+
+        }
+        memcpy(&g_fuse_lidar, &fuse_lidar, sizeof(str_lidar_info_to_fuse));
+        g_proc_flag = 1;
+        usleep(1000);
+    }
+}
+
+static void *ntzx_fuse_data_proc(void *arg)
+{
+    int frame_id = 0;
+    while (1) {
+        if (g_proc_flag == 0) {
+            usleep(1000);
+            continue;
+        }
+        /* 融合处理，由于该系统只有一个激光雷达判断所以没有融合部分代码 */
+
+        /* 数据写入 */
+        frame_id++;
+        g_grid_fuse_to_pl.frame_id = frame_id;
+        pthread_mutex_lock(&g_mutex_to_pl);
+        memcpy(g_grid_fuse_to_pl.gridmask, g_fuse_lidar.gridmask, LIDAR_VERTICAL_GRID_NUM*LIDAR_HORIZONTAL_GRID_NUM);
+        pthread_mutex_unlock(&g_mutex_to_pl);
+        g_proc_flag = 0;
+    }
+}
+
+/* 获取当前雷达数据点 */
+int ntzx_get_grid_data(str_grid_fuse_to_pl *fuse_to_pl)
+{
+    if (fuse_to_pl->frame_id != g_grid_fuse_to_pl.frame_id) 
+    {
+        if(pthread_mutex_trylock(&g_mutex_to_pl) ==0) //成功加锁
+        {
+            memcpy(fuse_to_pl, &g_grid_fuse_to_pl, sizeof(str_grid_fuse_to_pl));
+            pthread_mutex_unlock(&g_mutex_to_pl);           
+            return NTZX_FUSE_SUCCESS;
+        }      
+       else{
+           return NTZX_FUSE_TRYLOCK_FAIL;
+       }       
+    } 
+    else {
+        return NTZX_FUSE_INCOMPLETE;
+    }
+}
